716d66d2f909fdf8b93cb154d8c94dfe9583c598
Tayfun Bolat <tayfun.bolat@ecozum.com>
2020-06-20 10:45:38 +0300

    How to Create Self Hosting Services
    Self-Host Servis nasıl oluşturulur ? Çalışması ile ilgili ufak örnek bir proje

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SelfHosting.API.WorkerService;
+using SelfHosting.Services;
+
+namespace SelfHosting.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+          Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                //İlk olarak joblarımızı alabilmek için API ' yi ayağa kaldırıyoruz.
+                webBuilder.UseStartup<Startup>();  //.UseUrls();
+            })
+           .ConfigureServices((hostContext, services) =>
+           {
+  
+               services.AddHostedService<SchedulerWorkerService>(); //Hosted Servisimizi ekliyoruz.
+
+           }).UseWindowsService(); //Windows Servis olarak çalışacağını belirtiyoruz.
+    }
+}
---
+++ WorkerService/SchedulerWorkerService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using SelfHosting.API.AppSettings;
+using SelfHosting.Services;
+using Serilog;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SelfHosting.API.WorkerService
+{
+
+    /// <summary>
+    /// SchedulerWorkerService adında bir sınıf oluşturup, BackGroundService'den kalıtım alıyorum. 
+    /// </summary>
+    public class SchedulerWorkerService : BackgroundService
+    {
+        private readonly ISchedulerService _schedulerService;
+        private readonly ConfigParameter configParameter; //Self-Host Api appsetting.json dosyasından url ve method adlarını alıyoruz.
+        public SchedulerWorkerService(ISchedulerService schedulerService,
+            IOptions<ConfigParameter> options)
+        {
+            configParameter = options.Value;
+            _schedulerService =  schedulerService;
+        }
+        /// <summary>
+        /// Execute Methodunu override ederek ihtiyacımıza göre çalıştırıyoruz.
+        /// </summary>
+        /// <param name="stoppingToken"></param>
+        /// <returns></returns>
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                Log.Information("Worker Servis Çalıştı");
+
+                await _schedulerService.ExecuteScheduler(configParameter);
+
+                await Task.Delay(TimeSpan.FromMinutes(5), stoppingToken);
+            }
+        }
+    }
+}
